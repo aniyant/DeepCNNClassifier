15d16b2cec90a6ae3232c2919526fd3642af3dd1
aniyant <kumarsunny30066@gmail.com>
2022-09-25 16:24:19 +0530

    updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,4 @@
-echo [$(date)]: "START" 
+xitecho [$(date)]: "START" 
 echo [$(date)]: "creating env with python 3.8 version" 
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the environment" 
